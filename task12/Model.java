a0fb44390e8cd4c10bfb988dbb34827c726857dc
Jolly <maria.salogub@mail.ru>
2020-10-16 10:05:57 +0300

    task 12

---
+++ task12/Model.java
@@ -0,0 +1,14 @@
+package task12;
+
+public class Model {
+    public long calculation(long a, long b, String operator){
+        switch (operator){
+            case "+": return a+b;
+            case "-": return a-b;
+            case "*": return a*b;
+            case "/": if (b == 0) return 0;
+            return a/b;
+        }
+         return 0;
+    }
+}
